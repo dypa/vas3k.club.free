4496cd5f669debde76970ebd07150c4521b4503d
dypa <dypa@users.noreply.github.com>
2024-03-23 22:03:38 +0300

    обновление библиотек изменение подхода к раздаче статики

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 docker compose up -d --build
-
+docker compose run --rm bun
 docker exec vas3k-club-reader-api composer install
 docker exec vas3k-club-reader-api ./bin/console doctrine:schema:update --complete --force
 
-#because host `web` resolves to external ip, WTF?!
-#see nginx conf for description
-docker exec vas3k-club-reader-nginx nginx -s reload
-
-x-www-browser "$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vas3k-club-reader-nginx):3000/"
+# x-www-browser "$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vas3k-club-reader-nginx):3000/"
