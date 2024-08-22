ca9eb875f27ed7d9b2ba8cd97a019bb2f6bcfbcf
dypa <dypa@users.noreply.github.com>
2024-08-22 11:16:38 +0300

    опция больше не требуется

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,6 @@ docker compose up -d --build
 
 docker compose run --rm bun
 docker exec vas3k-club-reader-api composer install
-docker exec vas3k-club-reader-api ./bin/console doctrine:schema:update --complete --force
+docker exec vas3k-club-reader-api ./bin/console doctrine:schema:update --force
 
 #x-www-browser "$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vas3k-club-reader-nginx):3000/"
