05e92de58a17337a2d9c66c33927a971fd0fc7e2
dypa <dypa@users.noreply.github.com>
2024-09-21 10:31:10 +0300

    обновление библиотек

--- bin/update.sh
+++ bin/update.sh
@@ -1,4 +1,5 @@
 docker compose run --detach dev-server 
 docker compose exec dev-server bun run update
 docker compose exec api composer update
-docker compose stop dev-server 
+docker compose stop dev-server
+docker compose exec api composer outdated
