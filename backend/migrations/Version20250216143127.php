671972c63a62e35eb32883afd92d6229c7d13404
dypa <dypa@users.noreply.github.com>
2025-02-16 23:04:13 +0300

    уменьшение размера бд

---
+++ backend/migrations/Version20250216143127.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250216143127 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SQLitePlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SQLitePlatform'."
+        );
+
+        $this->connection->executeQuery('DROP TABLE IF EXISTS search');
+        $this->connection->executeQuery('CREATE VIRTUAL TABLE IF NOT EXISTS search USING fts5(id, title, body, columnsize=0)');    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SQLitePlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SQLitePlatform'."
+        );
+
+        $this->connection->executeQuery('DROP TABLE search');
+    }
+}
