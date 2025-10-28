63e409e13416d7caafa1be336fdb9ea8d5b16126
dypa <dypa@users.noreply.github.com>
2025-10-28 20:00:41 +0300

    doctrine:query:sql больше не существует зеркало yandex для alpine обновление библиотек

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,9 @@
-docker compose exec api ./bin/console doctrine:query:sql "VACUUM"
+docker compose exec api ./bin/console dbal:run-sql "VACUUM"
 docker compose run --rm --remove-orphans dev-server bun run update
 docker compose run --rm --remove-orphans api composer update --with-all-dependencies
 docker compose run --rm --remove-orphans api composer cs-fix
+docker compose exec api ./bin/console cache:clear
+docker compose exec api ./bin/console cache:warmup
 
 #https://github.com/composer/composer/issues/12254
 #docker compose run --rm --remove-orphans api composer outdated
